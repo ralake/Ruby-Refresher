e51565bf4c1f782c3cb682feda45d128416e8c0b
Richard Lake <ralake1985@gmail.com>
2014-12-13 09:48:20 +0000

    pass sixth, seventh, eighth and ninth test

--- lib/questions.rb
+++ lib/questions.rb
@@ -35,15 +35,19 @@ end
 # discard the first 3 elements of an array, 
 # e.g. [1, 2, 3, 4, 5, 6] becomes [4, 5, 6]
 def all_elements_except_first_3(array)
+	array.drop(3)
 end
 
 # add an element to the beginning of an array
 def add_element_to_beginning_of_array(array, element)
+	array.unshift(element)
 end
 
 # sort an array of words by their last letter, e.g.
 # ['sky', 'puma', 'maker'] becomes ['puma', 'maker', 'sky']
 def array_sort_by_last_letter_of_word(array)
+	(array.each { |word| word.reverse! }).sort!.each { |word| word.reverse! }
+	# x.each { |word| word.reverse! }
 end
 
 # cut strings in half, and return the first half, e.g.
--- spec/questions_spec.rb
+++ spec/questions_spec.rb
@@ -40,20 +40,20 @@ describe 'the Friday test :)' do
     expect(sorted).to eq [['Bob', 'Clive'], ['Bob', 'Dave'], ['Clive', 'Dave']]
   end
 
-#   it 'all_elements_except_first_3' do
-#     n = all_elements_except_first_3 [1, 2, 3, 4, 5, 6, 7]
-#     expect(n).to eq [4, 5, 6, 7]
-#   end
+  it 'all_elements_except_first_3' do
+    n = all_elements_except_first_3 [1, 2, 3, 4, 5, 6, 7]
+    expect(n).to eq [4, 5, 6, 7]
+  end
 
-#   it 'add_element_to_beginning_of_array' do
-#     n = add_element_to_beginning_of_array [2, 3, 4, 5], 1
-#     expect(n).to eq [1, 2, 3, 4, 5]
-#   end
+  it 'add_element_to_beginning_of_array' do
+    n = add_element_to_beginning_of_array [2, 3, 4, 5], 1
+    expect(n).to eq [1, 2, 3, 4, 5]
+  end
 
-#   it 'array_sort_by_last_letter_of_word' do
-#     n = array_sort_by_last_letter_of_word ['sky', 'puma', 'maker']
-#     expect(n).to eq ['puma', 'maker', 'sky']
-#   end
+  it 'array_sort_by_last_letter_of_word' do
+    n = array_sort_by_last_letter_of_word ['sky', 'puma', 'maker']
+    expect(n).to eq ['puma', 'maker', 'sky']
+  end
 
 #   it 'get_first_half_of_string' do
 #     a = get_first_half_of_string 'banana'
