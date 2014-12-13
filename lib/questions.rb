e3f36ee090201e414feca5004a605d8b64d8bd7e
Richard Lake <ralake1985@gmail.com>
2014-12-13 10:56:13 +0000

    pass tenth, eleventh and twelth test

--- lib/questions.rb
+++ lib/questions.rb
@@ -10,7 +10,7 @@ end
 
 # remove instances of nil (but NOT false) from an array
 def remove_nils_from_array(array)
-	array.select { |element| !element.nil? }
+	array.compact 
 end
 
 # remove instances of nil AND false from an array
@@ -47,18 +47,23 @@ end
 # ['sky', 'puma', 'maker'] becomes ['puma', 'maker', 'sky']
 def array_sort_by_last_letter_of_word(array)
 	(array.each { |word| word.reverse! }).sort!.each { |word| word.reverse! }
-	# x.each { |word| word.reverse! }
 end
 
 # cut strings in half, and return the first half, e.g.
 # 'banana' becomes 'ban'. If the string is an odd number of letters
 # round up - so 'apple' becomes 'app'
 def get_first_half_of_string(string)
+	string.slice(0, ((string.length).to_f / 2).ceil)
 end
 
 # turn a positive integer into a negative integer. A negative integer
 # stays negative
 def make_numbers_negative(number)
+	if number < 0
+		number
+	else
+		number * -1
+	end
 end
 
 # turn an array of numbers into two arrays of numbers, one an array of 
@@ -66,6 +71,9 @@ end
 # even numbers come first
 # so [1, 2, 3, 4, 5, 6] becomes [[2, 4, 6], [1, 3, 5]]
 def separate_array_into_even_and_odd_numbers(array)
+	new_array = []
+	new_array << array.select { |num| num.even? }
+	new_array << array.select { |num| num.odd? }
 end
 
 # count the numbers of elements in an element which are palindromes
